c6fe7ac93af9bf14968b2290ac58afeb70c46a9a
JinHui <hui.jin@boatchina.net>
2017-10-24 09:17:38 +0800

    test

--- app/src/main/java/com/example/jh/fencedemo/MainActivity.java
+++ app/src/main/java/com/example/jh/fencedemo/MainActivity.java
@@ -1,15 +1,26 @@
 package com.example.jh.fencedemo;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.amap.api.fence.GeoFence;
 import com.amap.api.fence.GeoFenceClient;
+import com.amap.api.fence.GeoFenceListener;
 import com.amap.api.location.AMapLocationClient;
 import com.amap.api.location.AMapLocationClientOption;
 import com.amap.api.maps.AMap;
@@ -27,7 +38,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, CompoundButton.OnCheckedChangeListener, GeoFenceListener {
 
 
     private View lyOption;
@@ -116,6 +127,122 @@ public class MainActivity extends AppCompatActivity {
 //            mAMap.moveCamera(CameraUpdateFactory.zoomBy(6));
             setUpMap();
         }
+        btOption.setVisibility(View.VISIBLE);
+        btOption.setText(getString(R.string.hideOption));
+        resetView_round();
+
+        btAddFence.setOnClickListener(this);
+        btOption.setOnClickListener(this);
+        cbAlertIn.setOnCheckedChangeListener(this);
+        cbAlertOut.setOnCheckedChangeListener(this);
+        cbAldertStated.setOnCheckedChangeListener(this);
+
+        IntentFilter filter = new IntentFilter(
+                ConnectivityManager.CONNECTIVITY_ACTION);
+        filter.addAction(GEOFENCE_BROADCAST_ACTION);
+        registerReceiver(mGeoFenceReceiver, filter);
+
+
+        /**
+         * 创建pendingIntent
+         */
+        fenceClient.createPendingIntent(GEOFENCE_BROADCAST_ACTION);
+        fenceClient.setGeoFenceListener(this);
+        /**
+         * 设置地理围栏的触发行为,默认为进入
+         */
+        fenceClient.setActivateAction(GeoFenceClient.GEOFENCE_IN);
+    }
+
+    Handler handler = new Handler() {
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case 0 :
+                    StringBuffer sb = new StringBuffer();
+                    sb.append("添加围栏成功");
+                    String customId = (String)msg.obj;
+                    if(!TextUtils.isEmpty(customId)){
+                        sb.append("customId: ").append(customId);
+                    }
+                    Toast.makeText(getApplicationContext(), sb.toString(),
+                            Toast.LENGTH_SHORT).show();
+                    drawFence2Map();
+                    break;
+                case 1 :
+                    int errorCode = msg.arg1;
+                    Toast.makeText(getApplicationContext(),
+                            "添加围栏失败 " + errorCode, Toast.LENGTH_SHORT).show();
+                    break;
+                case 2 :
+                    String statusStr = (String) msg.obj;
+                    tvResult.setVisibility(View.VISIBLE);
+                    tvResult.append(statusStr + "\n");
+                    break;
+                default :
+                    break;
+            }
+        }
+    };
+
+    private void drawFence2Map() {
+    }
+
+    /**
+     * 接收触发围栏后的广播,当添加围栏成功之后，会立即对所有围栏状态进行一次侦测，如果当前状态与用户设置的触发行为相符将会立即触发一次围栏广播；
+     * 只有当触发围栏之后才会收到广播,对于同一触发行为只会发送一次广播不会重复发送，除非位置和围栏的关系再次发生了改变。
+     */
+    private BroadcastReceiver mGeoFenceReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            // 接收广播
+            if (intent.getAction().equals(GEOFENCE_BROADCAST_ACTION)) {
+                Bundle bundle = intent.getExtras();
+                String customId = bundle
+                        .getString(GeoFence.BUNDLE_KEY_CUSTOMID);
+                String fenceId = bundle.getString(GeoFence.BUNDLE_KEY_FENCEID);
+                //status标识的是当前的围栏状态，不是围栏行为
+                int status = bundle.getInt(GeoFence.BUNDLE_KEY_FENCESTATUS);
+                StringBuffer sb = new StringBuffer();
+                switch (status) {
+                    case GeoFence.STATUS_LOCFAIL :
+                        sb.append("定位失败");
+                        break;
+                    case GeoFence.STATUS_IN :
+                        sb.append("进入围栏 ");
+                        break;
+                    case GeoFence.STATUS_OUT :
+                        sb.append("离开围栏 ");
+                        break;
+                    case GeoFence.STATUS_STAYED :
+                        sb.append("停留在围栏内 ");
+                        break;
+                    default :
+                        break;
+                }
+                if(status != GeoFence.STATUS_LOCFAIL){
+                    if(!TextUtils.isEmpty(customId)){
+                        sb.append(" customId: " + customId);
+                    }
+                    sb.append(" fenceId: " + fenceId);
+                }
+                String str = sb.toString();
+                Message msg = Message.obtain();
+                msg.obj = str;
+                msg.what = 2;
+                handler.sendMessage(msg);
+            }
+        }
+    };
+
+
+
+
+    private void resetView_round() {
+        etRadius.setHint("围栏半径");
+        etRadius.setVisibility(View.VISIBLE);
+        tvGuide.setBackgroundColor(getResources().getColor(R.color.red));
+        tvGuide.setText("请点击地图选择围栏的中心点");
+        tvGuide.setVisibility(View.VISIBLE);
     }
 
     /**
@@ -166,5 +293,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         mMapView.onDestroy();
+    }
+
+    @Override
+    public void onClick(View v) {
+        
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        
+    }
+
+    @Override
+    public void onGeoFenceCreateFinished(List<GeoFence> list, int i, String s) {
+        
     }
 }
